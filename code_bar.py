b8f16f90be40055c33ad8ff7a873a518a53d09b2
nabiladigo <nabiladigo91@gmail.com>
2022-02-02 22:35:02 -0500

    the code is working

--- code_bar.py
+++ code_bar.py
@@ -14,6 +14,7 @@ class Instructor(Member):
     def __init__(self, full_name, bio):
         super().__init__(full_name)
         self.bio= bio
+        self.skills =[]
     def add_skill(self, new_skill):
         self.skills.append(new_skill)
 
@@ -40,3 +41,20 @@ class Workshop():
         print("Instructor:")
         for i, instructor in enumerate(self.instructor):
             print(f"{i+1}. {instructor.full_name} - {instructor.skills}\n and {instructor.bio} ")
+
+
+workshop = Workshop("12/03/2014", "Shutl")
+
+jane = Student("Jane Doe", "I am trying to learn programming and need some help")
+lena = Student("Lena Smith", "I am really excited about learning to program!")
+vicky = Instructor("Vicky Python", "I want to help people learn coding.")
+vicky.add_skill("HTML")
+vicky.add_skill("JavaScript")
+nicole = Instructor("Nicole McMillan", "I have been programming for 5 years in Python and want to spread the love")
+nicole.add_skill("Python")
+
+workshop.add_participant(jane)
+workshop.add_participant(lena)
+workshop.add_participant(vicky)
+workshop.add_participant(nicole)
+workshop.print_details()

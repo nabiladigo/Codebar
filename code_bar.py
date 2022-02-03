826039d995f756aed81f375be1c5fb3c62b77b8e
nabiladigo <nabiladigo91@gmail.com>
2022-02-02 22:20:19 -0500

    create Members

---
+++ code_bar.py
@@ -0,0 +1,10 @@
+class Member:
+    def __init__(self, full_name):
+        self.full_name = full_name
+        
+    def greet(self):
+        print(f"Hi, my name is {self.full_name}!")
+
+# bella= Member("Nabila Digourdi")
+# bella.greet()
+        

14bccb420cd98304b2e337eb9a73a922baa9e300
nabiladigo <nabiladigo91@gmail.com>
2022-02-02 22:29:32 -0500

    add skills and create workshop

--- code_bar.py
+++ code_bar.py
@@ -14,3 +14,12 @@ class Instructor(Member):
     def __init__(self, full_name, bio):
         super().__init__(full_name)
         self.bio= bio
+    def add_skill(self, new_skill):
+        self.skills.append(new_skill)
+
+class Workshop():
+    def __init__(self, date, subject):
+        self.date = date
+        self.subject = subject
+        self.student =[]
+        self.instructor = []
